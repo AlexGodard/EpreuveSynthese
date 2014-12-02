a3a64218f00763f8203c746e36aa3d18bade7a3c
dcantincstj <davidcantin15@gmail.com>
2014-12-02 14:42:31 -0500

    runes

--- site/Inox Fureteur/js/setRunes.js
+++ site/Inox Fureteur/js/setRunes.js
@@ -1,17 +1,27 @@
+$(document).ready(function() {
 var URL = "https://tpsynthese-web-v5458545875-mlarameecstj.c9.io/runes";
-	$("#btnLogin").click(function() { 
+var runesBD;
+	$("#air").click(function() {
+	
 		$.ajax({
 			url : URL,
 			type : 'GET',
 			dataType : 'json',
 			success: function(response) {
-			    localStorage.setItem("token",response.token.token);
-			    window.location.reload("index.html");		
-			    return true;						   
+		        runesBD = response;
+			    return true;				
 			},
-			error: function(response) {			
+			error: function(runes) {			
 			   alert("Veuillez verifier votre nom d'utilisateur et votre mot de passe");
 			   return false;						   
 			}
 		});	
-}); 
+		
+		alert(runesBD.air);
+		
+		
+		
+		
+		
+     }); 
+});
